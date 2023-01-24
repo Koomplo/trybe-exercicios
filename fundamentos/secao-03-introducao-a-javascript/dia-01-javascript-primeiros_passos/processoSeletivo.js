b187ad9c0584f46bc248c75b3890d9bf69b481dd
Koomplo <mariomarcio12324@gmail.com>
2023-01-24 14:18:23 -0300

    Exercicio processoSeletivo fundamentos/3.1

--- fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros_passos/processoSeletivo.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros_passos/processoSeletivo.js
@@ -9,7 +9,7 @@ switch(resultados) {
       console.log("Você está na nossa lista de espera");
       break;
     case 'reprovada':
-      console.log("Você foi reprovada(o)");
+      console.log("Você foi reprovada(o) :'(");
       break;
     default:
       console.log("Informação incorreta");
