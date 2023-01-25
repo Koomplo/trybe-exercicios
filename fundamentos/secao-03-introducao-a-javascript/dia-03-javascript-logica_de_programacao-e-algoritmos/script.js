6e1d323e920800e7d83dd9a1df6acfb4f79b95a8
Koomplo <mariomarcio12324@gmail.com>
2023-01-25 17:59:54 -0300

    Exercicios dinamicos fundamentos/3.3

--- fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-logica_de_programacao-e-algoritmos/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-logica_de_programacao-e-algoritmos/script.js
@@ -1,4 +1,4 @@
-let novasPessoasUsuarias = ['Pessoa1', 'Pessoa2', 'Pessoa3', 'Pessoa4', 'Pessoa5'];
+let novasPessoasUsuarias = ['Érica', 'Silvia', 'Lucas', 'Felipe', 'Igor'];
 
 let sistemas = ['Folha de Pagamento', 'RH', 'Treinamento', 'Slack', 'Zoom']
 
@@ -18,4 +18,21 @@ if (login === 'usuarioTrybe' && senha === 'trybe2023' ){
     console.log('Acesso Permitido!');
 }else{
     console.log('Acesso negado!');
+}
+
+
+// Sistema do 'For' para verificar se tem acesso?
+// Passar pelo array do sistema?
+// Como passar pelo array dos usuários?
+
+
+if(temAcesso === true) {
+    for(let indexSistemas = 0; indexSistemas < sistemas.length; indexSistemas += 1) {
+        console.log("Sistemas: ", sistemas[indexSistemas])
+    }
+    for(let indexPessoas = 0; indexPessoas < novasPessoasUsuarias.length; indexPessoas += 1){
+        console.log(novasPessoasUsuarias[indexPessoas])
+    }
+
+
 }
