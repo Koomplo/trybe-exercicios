4c661262d5c8518414614a597d760b076726b7cf
Koomplo <mariomarcio12324@gmail.com>
2023-01-25 16:46:03 -0300

    Exercicio software login, primeira etapa

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-logica_de_programacao-e-algoritmos/script.js
@@ -0,0 +1,21 @@
+let novasPessoasUsuarias = ['Pessoa1', 'Pessoa2', 'Pessoa3', 'Pessoa4', 'Pessoa5'];
+
+let sistemas = ['Folha de Pagamento', 'RH', 'Treinamento', 'Slack', 'Zoom']
+
+// Objetivo: Criar um software para adicionar novas pessoas usuárias.
+
+
+
+
+
+
+let login = 'usuarioTrybe';
+let senha = 'trybe2023';
+let temAcesso = false;
+
+if (login === 'usuarioTrybe' && senha === 'trybe2023' ){
+    temAcesso = true;
+    console.log('Acesso Permitido!');
+}else{
+    console.log('Acesso negado!');
+}
