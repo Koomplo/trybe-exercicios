c672b463a01460ee4309b48e0d441a28e0f3a2bb
Koomplo <mariomarcio12324@gmail.com>
2023-01-26 17:05:07 -0300

    Exercicios fundamentos/3.4 (Usando funções)

--- fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/script.js
@@ -7,3 +7,12 @@ function sum(numero1, numero2){
 console.log(sum(2, 5));
 console.log(sum(10, 9));
 console.log(sum(8, 11));
+
+//função calcular numeros de um jogo de vôlei
+
+function calcularPontos(vitorias, empates) {
+    let resultado;
+    resultado = (vitorias * 2) + empates;
+    return resultado;
+}
+console.log(calcularPontos(10,2));
