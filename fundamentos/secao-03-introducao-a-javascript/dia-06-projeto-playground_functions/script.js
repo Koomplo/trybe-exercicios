33861496a70c0c42eac63f775a5612b35ab18f88
Koomplo <mariomarcio12324@gmail.com>
2023-01-27 23:23:52 -0300

    Adicionado função footballPoints com arrowFunction e seus respectivos console.logs

--- fundamentos/secao-03-introducao-a-javascript/dia-06-projeto-playground_functions/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-06-projeto-playground_functions/script.js
@@ -28,3 +28,17 @@ function concatName(array) {
   }
 
 // console.log(concatName(["Mario", "Josue", "Ivan", "Felipe", "Layla"]));
+
+// Crie uma função que calcula a quantidade de pontos em um campeonato de futebol
+// Implemente a função footballPoints que calcula a pontuação de um time de futebol
+// em um campeonato a partir do numero de vitórias (3 pts) ou empate (1 pts)
+
+const footballPoints = (wins, ties) => (wins * 3) + (ties * 1);
+
+// console.log(footballPoints(14, 8));
+// console.log(footballPoints(1, 2));
+// console.log(footballPoints(0, 0));
+
+// Crie uma função que calcula o número de repetições do maior número.
+// Imeplemente a função highestCount que deverá retornar a quantidade de vezes que o maior número se repete um array de números.
+
