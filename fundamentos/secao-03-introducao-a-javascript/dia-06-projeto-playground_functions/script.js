5c62367c6d08e939c61a93da17c96beeab73d303
Koomplo <mariomarcio12324@gmail.com>
2023-01-27 21:59:10 -0300

    IIniciando o Projeto Playground Functions

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-06-projeto-playground_functions/script.js
@@ -0,0 +1,9 @@
+// Crie uma função usando o operador &&
+// Implemente a função compareTrue utiulizando somente o operador &&
+
+const compareTrue = (param1, param2) => param1 && param2;
+const girafa = true
+const elefante = true
+const macaco = false
+
+console.log(param1 = girafa , param2 = macaco);
