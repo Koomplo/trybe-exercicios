87df1161843eb2cb8049bcd91292a40ec2a351f5
Koomplo <mariomarcio12324@gmail.com>
2023-01-27 22:20:58 -0300

    Adicionado função concatName e adicionado console log com explicação e teste de cada função

--- fundamentos/secao-03-introducao-a-javascript/dia-06-projeto-playground_functions/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-06-projeto-playground_functions/script.js
@@ -6,9 +6,25 @@ const girafa = true
 const elefante = true
 const macaco = false
 
+// console.log(compareTrue(girafa, elefante))
+// console.log(compareTrue(girafa, macaco))
+
 // Crie uma função que divida uma frase
-// Implemente a função ''splitSentence que divide uma frase de acordo com a quantidade de palavras
+// Implemente a função ''splitSentence que divide uma frase de acordo com a quantidade de palavras.
 
 function splitSentence(sentence) {
     return sentence.split(" ");
   }
+
+// console.log(splitSentence('Go Trybe'));
+// console.log(splitSentence('vamo que vamo'));
+// console.log(splitSentence('foguete'));
+
+// Crie uma função que use concatenação de strings
+// Implemente a função concatName que recebe um array de strings e retorna uma string com o último e o primeiro item.
+
+function concatName(array) {
+    return array[array.length - 1] + " " + array[0];
+  }
+
+// console.log(concatName(["Mario", "Josue", "Ivan", "Felipe", "Layla"]));
