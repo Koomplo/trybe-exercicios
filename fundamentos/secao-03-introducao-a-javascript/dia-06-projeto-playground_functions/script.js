90fa1044408de3558d7f7330e021ded9cb31220a
Koomplo <mariomarcio12324@gmail.com>
2023-01-27 22:06:02 -0300

    Projeto Playground Functions, adicionado função splitSentence

--- fundamentos/secao-03-introducao-a-javascript/dia-06-projeto-playground_functions/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-06-projeto-playground_functions/script.js
@@ -1,9 +1,14 @@
 // Crie uma função usando o operador &&
 // Implemente a função compareTrue utiulizando somente o operador &&
 
-const compareTrue = (param1, param2) => param1 && param2;
+const compareTrue = (test1, test2) => test1 && test2;
 const girafa = true
 const elefante = true
 const macaco = false
 
-console.log(param1 = girafa , param2 = macaco);
+// Crie uma função que divida uma frase
+// Implemente a função ''splitSentence que divide uma frase de acordo com a quantidade de palavras
+
+function splitSentence(sentence) {
+    return sentence.split(" ");
+  }
