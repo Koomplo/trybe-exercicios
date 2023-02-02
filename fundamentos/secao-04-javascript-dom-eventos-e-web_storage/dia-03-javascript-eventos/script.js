291a3b31ed85044f880a753d1533e17d85bf8c6f
Koomplo <mariomarcio12324@gmail.com>
2023-02-02 17:35:45 -0300

    Fundamentos/4.3, botão de start concluido. corrida e finalização concluida.

--- fundamentos/secao-04-javascript-dom-eventos-e-web_storage/dia-03-javascript-eventos/script.js
+++ fundamentos/secao-04-javascript-dom-eventos-e-web_storage/dia-03-javascript-eventos/script.js
@@ -17,15 +17,26 @@ window.onload = () => {
     resetBtn.addEventListener('click', resetCars);
 
     startBtn.addEventListener('click', () => {
-        player1.style.marginLeft = parseInt(player1.style.marginLeft) + Math.random() * 300 + "px";
-        player2.style.marginLeft = parseInt(player2.style.marginLeft) + Math.random() * 300 + "px";
+        player1.style.marginLeft = parseInt(player1.style.marginLeft) + Math.random() * 100 + "px";
+        player2.style.marginLeft = parseInt(player2.style.marginLeft) + Math.random() * 100 + "px";
 
 // Corrida Iniciada
 // Criando competição
         
-    const player1Win = parseInt(player1.style.marginLeft) > innerWidth;
-    const playe2Win = parseInt(player2.style.marginLeft) > innerHeight;
-    
+    const player1Win = parseInt(player1.style.marginLeft) > window.innerWidth;
+    const playe2Win = parseInt(player2.style.marginLeft) > window.innerHeight;
 
-    })
+    if (player1Win) {
+        alert('PLAYER 1 GANHOU!');
+        audioWinner.play();
+        resetCars();
+    }
+
+    if (playe2Win) {
+        alert('PLAYER 2 GANHOU!');
+        audioWinner.play();
+        resetCars();
+    }
+
+    } )
 }
