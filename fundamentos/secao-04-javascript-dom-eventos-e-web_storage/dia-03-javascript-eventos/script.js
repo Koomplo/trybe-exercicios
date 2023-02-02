d5dc8d220a2f9f509cbe1719d533a45e8ab2bcac
Koomplo <mariomarcio12324@gmail.com>
2023-02-02 16:56:22 -0300

    Fundamentos/4.3, declarada variaveis e feito botão reset

--- fundamentos/secao-04-javascript-dom-eventos-e-web_storage/dia-03-javascript-eventos/script.js
+++ fundamentos/secao-04-javascript-dom-eventos-e-web_storage/dia-03-javascript-eventos/script.js
@@ -1,3 +1,18 @@
 window.onload = () => {
-    
+
+    const startBtn = document.getElementById('start-race-btn');
+    const resetBtn = document.getElementById('reset-race-btn');
+    const player1 = document.getElementById('player1');
+    const player2 = document.getElementById('player2');
+    const audioWinner = document.getElementById('audioWinner');
+
+    player1.style.marginLeft = 0;
+    player2.style.marginLeft = 0;
+
+    const resetCars = () => {
+        player1.style.marginLeft = 0;
+        player2.style.marginLeft = 0;
+    };
+
+    resetBtn.addEventListener('click', resetCars);
 }
