4719f3c7ae6d0a486e610a8ba324e28db1ccdc58
Koomplo <mariomarcio12324@gmail.com>
2023-02-06 07:15:46 -0300

    Adicionado localStorage e função com verificação da paleta de cores se já existe ou não.

--- fundamentos/secao-04-javascript-dom-eventos-e-web_storage/dia-05-projeto-arte_com_pixels/script.js
+++ fundamentos/secao-04-javascript-dom-eventos-e-web_storage/dia-05-projeto-arte_com_pixels/script.js
@@ -3,17 +3,29 @@ const cor1 = document.querySelectorAll(".color")[1];
 const cor2 = document.querySelectorAll(".color")[2];
 const cor3 = document.querySelectorAll(".color")[3];
 
-        button.addEventListener("click", function() {
+    button.addEventListener("click", function() {
+        cor1.style.backgroundColor = getRandomColor();
+        cor2.style.backgroundColor = getRandomColor();
+        cor3.style.backgroundColor = getRandomColor();
+        localStorage.setItem("colorPalette", JSON.stringify([cor1.style.backgroundColor, cor2.style.backgroundColor, cor3.style.backgroundColor]));
+     });
+
+     if (localStorage.getItem("colorPalette")) {
+        const colorPalette = JSON.parse(localStorage.getItem("colorPalette"));
+            cor1.style.backgroundColor = colorPalette[0];
+            cor2.style.backgroundColor = colorPalette[1];
+            cor3.style.backgroundColor = colorPalette[2];
+        } else {
             cor1.style.backgroundColor = getRandomColor();
             cor2.style.backgroundColor = getRandomColor();
             cor3.style.backgroundColor = getRandomColor();
-        });
+        }
 
-        function getRandomColor() {
-            const letters = "0123456789ABCDEF";
-            let cor = "#";
-            for (let index = 0; index < 6; index += 1) {
-              cor += letters[Math.floor(Math.random() * 16)];
-            }
-            return cor;
-          }
+    function getRandomColor() {
+        const letters = "0123456789ABCDEF";
+        let cor = "#";
+        for (let index = 0; index < 6; index += 1) {
+            cor += letters[Math.floor(Math.random() * 16)];
+         }
+        return cor;
+    }
