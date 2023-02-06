791ccd2103ed745d51fa82e1a7343657b019ca46
Koomplo <mariomarcio12324@gmail.com>
2023-02-06 07:07:25 -0300

    Projeto 4.5, inicio, criada paleta de cores aleatória.

--- fundamentos/secao-04-javascript-dom-eventos-e-web_storage/dia-05-projeto-arte_com_pixels/script.js
+++ fundamentos/secao-04-javascript-dom-eventos-e-web_storage/dia-05-projeto-arte_com_pixels/script.js
@@ -1,9 +1,19 @@
-var penColour = 'black';
-    
-function setPenColour(pen) {
-  penColour = pen;
-}
+const button = document.getElementById("button-random-color");
+const cor1 = document.querySelectorAll(".color")[1];
+const cor2 = document.querySelectorAll(".color")[2];
+const cor3 = document.querySelectorAll(".color")[3];
 
-function setPixelColour(pixel) {
-  pixel.style.backgroundColor = penColour;
-}
+        button.addEventListener("click", function() {
+            cor1.style.backgroundColor = getRandomColor();
+            cor2.style.backgroundColor = getRandomColor();
+            cor3.style.backgroundColor = getRandomColor();
+        });
+
+        function getRandomColor() {
+            const letters = "0123456789ABCDEF";
+            let cor = "#";
+            for (let index = 0; index < 6; index += 1) {
+              cor += letters[Math.floor(Math.random() * 16)];
+            }
+            return cor;
+          }
