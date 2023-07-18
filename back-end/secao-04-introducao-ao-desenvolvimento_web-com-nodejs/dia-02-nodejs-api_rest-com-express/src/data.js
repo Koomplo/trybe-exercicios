b2c9febd806a4dd0f550495d428c116819742717
mariomteixeira <mariomarcio12324@gmail.com>
2023-07-18 16:14:01 -0300

    Adicionando usuários utilizando método POST

---
+++ back-end/secao-04-introducao-ao-desenvolvimento_web-com-nodejs/dia-02-nodejs-api_rest-com-express/src/data.js
@@ -0,0 +1,6 @@
+const users = [
+    { name: 'João', age: 25 },
+    { name: 'Maria', age: 27 }
+];
+
+module.exports = users;
--- back-end/secao-04-introducao-ao-desenvolvimento_web-com-nodejs/dia-02-nodejs-api_rest-com-express/src/server.js
+++ back-end/secao-04-introducao-ao-desenvolvimento_web-com-nodejs/dia-02-nodejs-api_rest-com-express/src/server.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const users = require('./data');
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (request, response) => {
+app.get('/', (_request, response) => {
     console.log('Rota / acessada!');
     response.send('Respondendo a requisição GET /');
-})
+});
+
+app.get('/users', (_request, response) => {
+    response.status(200).send(users);
+});
+
+app.post('/users', (request, response) => {
+    const { name, age } = request.body;
+    const user = { name, age };
+    users.push(user);
+    response.status(201).json(user);
+});
 
 app.listen(3000, () => {
     console.log('Server on port', 3000);
