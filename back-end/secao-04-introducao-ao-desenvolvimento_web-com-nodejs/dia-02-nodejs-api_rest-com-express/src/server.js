3cd79f59f494b52561f7a236ecc38b9533c7f7b4
mariomteixeira <mariomarcio12324@gmail.com>
2023-07-18 11:26:36 -0300

    Adicionando rota GET

--- back-end/secao-04-introducao-ao-desenvolvimento_web-com-nodejs/dia-02-nodejs-api_rest-com-express/src/server.js
+++ back-end/secao-04-introducao-ao-desenvolvimento_web-com-nodejs/dia-02-nodejs-api_rest-com-express/src/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (request, response) => {
+    console.log('Rota / acessada!');
+    response.send('Respondendo a requisição GET /');
+})
+
 app.listen(3000, () => {
     console.log('Server on port', 3000);
 });
